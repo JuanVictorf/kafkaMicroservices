649dd84df976d0601e55b37b2bed98e68ea8e279
JuanVictorf <estudantejuan0@gmail.com>
2026-03-30 19:09:17 -0300

    feat: comunicação entre os microservices para validação

--- pedidos/src/main/java/io/github/juanvictorf/icompras/pedidos/config/ClientsConfig.java
+++ pedidos/src/main/java/io/github/juanvictorf/icompras/pedidos/config/ClientsConfig.java
@@ -4,6 +4,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(basePackages = " io.github.juanvictorf.icompras.pedidos.cliente")
+@EnableFeignClients(basePackages = "io.github.juanvictorf.icompras.pedidos.client")
 public class ClientsConfig {
 }
--- pedidos/src/main/java/io/github/juanvictorf/icompras/pedidos/validator/PedidoValidator.java
+++ pedidos/src/main/java/io/github/juanvictorf/icompras/pedidos/validator/PedidoValidator.java
@@ -1,15 +1,20 @@
 package io.github.juanvictorf.icompras.pedidos.validator;
 
+import feign.FeignException;
 import io.github.juanvictorf.icompras.pedidos.client.ClientsClient;
 import io.github.juanvictorf.icompras.pedidos.client.ProdutosClient;
+import io.github.juanvictorf.icompras.pedidos.client.representation.ClienteRepresentation;
+import io.github.juanvictorf.icompras.pedidos.client.representation.ProdutoRepresentation;
 import io.github.juanvictorf.icompras.pedidos.model.ItemPedido;
 import io.github.juanvictorf.icompras.pedidos.model.Pedido;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class PedidoValidator {
 
     private final ProdutosClient produtosClient;
@@ -22,10 +27,23 @@ public class PedidoValidator {
     }
 
     private void validarCliente(Long codigo){
+        try{
+            var response = clientsClient.obterDados(codigo);
+            ClienteRepresentation cliente = response.getBody();
+            log.info("Cliente de código {} encontrado: {} ", cliente.codigo(), cliente.nome());
+        } catch (FeignException.NotFound e){
+            log.error("Cliente não encontrado");
+        }
 
     }
 
     private void validarItem(ItemPedido item){
-
+        try{
+            var response = produtosClient.obterDados(item.getCodigoProduto());
+            ProdutoRepresentation produto = response.getBody();
+            log.info("Produto de código {} encontrado: {}", produto.codigo(), produto.nome());
+        } catch (FeignException.NotFound e) {
+            log.error("Produto não encontrado");
+        }
     }
 }
