dc4ea62a93fb8838ff3da59ee63de7b0371c1421
JuanVictorf <estudantejuan0@gmail.com>
2026-03-30 18:38:18 -0300

    feat: implementando o cliente do microservice de clientes

--- pedidos/src/main/java/io/github/juanvictorf/icompras/pedidos/validator/PedidoValidator.java
+++ pedidos/src/main/java/io/github/juanvictorf/icompras/pedidos/validator/PedidoValidator.java
@@ -1,6 +1,8 @@
 package io.github.juanvictorf.icompras.pedidos.validator;
 
+import io.github.juanvictorf.icompras.pedidos.client.ClientsClient;
 import io.github.juanvictorf.icompras.pedidos.client.ProdutosClient;
+import io.github.juanvictorf.icompras.pedidos.model.ItemPedido;
 import io.github.juanvictorf.icompras.pedidos.model.Pedido;
 import lombok.RequiredArgsConstructor;
 
@@ -11,7 +13,19 @@ import org.springframework.stereotype.Component;
 public class PedidoValidator {
 
     private final ProdutosClient produtosClient;
+    private final ClientsClient clientsClient;
 
-    public void validar(Pedido pedido) {
+    public void validar(Pedido pedido){
+        Long codigoCliente = pedido.getCodigoCliente();
+        validarCliente(codigoCliente);
+        pedido.getItens().forEach(this::validarItem);
+    }
+
+    private void validarCliente(Long codigo){
+
+    }
+
+    private void validarItem(ItemPedido item){
+
     }
 }
